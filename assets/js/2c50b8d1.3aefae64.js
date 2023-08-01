79284d0a7017421437a27c30017a98845de52a27
rdot117 <96553383+rdot117@users.noreply.github.com>
2023-07-31 17:59:17 -0700

    Built and published by Moonwave

---
+++ assets/js/2c50b8d1.3aefae64.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[8834],{5623:e=>{e.exports=JSON.parse('{"functions":[],"properties":[],"types":[],"name":"LegacyLoader","desc":"Legacy loading logic","private":true,"source":{"line":7,"path":"src/userinputserviceutils/node_modules/@quenty/loader/src/LegacyLoader.lua"}}')}}]);
---
+++ assets/js/76799532.47444490.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2773],{23725:e=>{e.exports=JSON.parse('{"functions":[],"properties":[],"types":[],"name":"promiseWait","desc":"Wraps the task.delay() API in a promise","source":{"line":6,"path":"src/switch/node_modules/@quenty/promise/src/Shared/Utility/promiseWait.lua"}}')}}]);
